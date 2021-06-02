e29e98d06ed497595a347a1c8ba0bb2d5ad241ac
Theo Cocco <theo@Theos-MacBook-Air.local>
2021-06-01 22:06:28 -0700

    almost finished builder

--- Restaurant/js/helperClasses/calendarHelper.js
+++ Restaurant/js/helperClasses/calendarHelper.js
@@ -1,14 +1,44 @@
 export class Calendar {
   checkHours(day, hour) {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
     switch (day) {
       case 0:
         console.log("We are not open Sunday");
         return false;
       case 5:
       case 6:
-        break;
+        if (hour >= 17 && hour <= 23) {
+          console.log(`We are open at ${hour - 12}pm on ${days[day]}`);
+          return true;
+        } else {
+          console.log(
+            `We are not open at ${
+              hour <= 12 ? hour + (hour === 12 ? "pm" : "am") : hour - 12 + "pm"
+            } on ${days[day]}`
+          );
+          return false;
+        }
       default:
-        break;
+        if (hour >= 17 && hour <= 21) {
+          console.log(`We are open at ${hour - 12} on ${days[day]}`);
+          return true;
+        } else {
+          console.log(
+            `We are not open at ${
+              hour <= 12 ? hour + (hour === 12 ? "pm" : "am") : hour - 12 + "pm"
+            } on ${days[day]}`
+          );
+          return false;
+        }
     }
   }
 }
--- Restaurant/js/helperClasses/reservationHelper.js
+++ Restaurant/js/helperClasses/reservationHelper.js
@@ -1,50 +1,57 @@
 class Reservation {
   static #existingReservations = [];
 
-  checkAvailability = (date, size) => {
+  #makeDate(day, hour) {
+    today = new Date();
+
+    res = new Date(today.getFullYear(), today.getMonth(), day, hour, 0, 0);
+
+    if (today.getDate() > res.getDate()) {
+      if (today.getMonth() === 11)
+        res.setFullYear(today.getFullYear() + 1, today.getMonth() + 1);
+      else res.setMonth(today.getMonth() + 1);
+    }
+
+    return res;
+  }
+
+  checkAvailability = (date, size = 0) => {
     let seatsTaken = 0;
 
     Reservation.#existingReservations
       .filter((r) => r.date === date)
       .forEach((r) => (seatsTaken += r.size));
 
-    return size + seatsTaken <= 20;
+    return size ? size + seatsTaken <= 20 : seatsTaken < 20;
   };
 
   constructor(name, { date, size }) {
     this.name = name;
     console.log(
-      `Thank you ${name} for starting a reservation, when would you like to make your reservation?`
+      `Thank you ${name} for starting a reservation, please enter the day of the month and the time you wish to set your reservation for.`
     );
     this.date = date;
     this.size = size;
   }
 
-  setDate(date) {
-    console.log(`Checking to see if we are open on ${date}`);
-    if (date in calendar) {
-      this.date = date;
-      console.log(`${date} is available, what size is  your party?`);
-    } else console.log(`${date} is unavailable`);
-    return this;
+  setDate(day, hour) {
+    if (calendar.checkHours(day, hour)) {
+      if (this.checkAvailability(this.#makeDate(day, hour))) {
+        console.log(`We still have seats available at `);
+        this.date = this.#makeDate(day, hour);
+      }
+    }
   }
 
   setSize(size) {
-    console.log(`Checking to see if we have room for ${size} people.`);
-
-    if (this.checkAvailability(this.date, size)) {
-      console.log(`We have room for ${size} people.`);
-      this.size = size;
-      return this;
-    } else
-      console.log(`I'm sorry we do not have enough room for ${size} people.`);
+    this.size = size;
   }
 
   build() {
     Reservation.#existingReservations.push({ date, size, instance: this });
 
     console.log(
-      `Thank you ${name}, your reservation is set for ${date} for ${size} people`
+      `Thank you ${this.name}, your reservation is set for ${this.date} for ${this.size} people`
     );
   }
 }
