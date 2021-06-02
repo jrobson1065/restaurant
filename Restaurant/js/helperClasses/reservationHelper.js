46f3b1ca5a0951e3889a4a507a50c86eaa6658cc
Theo Cocco <theo@Theos-MacBook-Air.local>
2021-06-01 17:39:10 -0700

    researching date time

--- Restaurant/js/helperClasses/reservationHelper.js
+++ Restaurant/js/helperClasses/reservationHelper.js
@@ -18,10 +18,6 @@ class Reservation {
     );
     this.date = date;
     this.size = size;
-    // console.log(
-    //   `Thank you ${name}, your reservation is set for ${date} for ${size} people`
-    // );
-    // Reservation.#existingReservations.push({ date, size, instance: this });
   }
 
   setDate(date) {
@@ -43,27 +39,12 @@ class Reservation {
     } else
       console.log(`I'm sorry we do not have enough room for ${size} people.`);
   }
+
+  build() {
+    Reservation.#existingReservations.push({ date, size, instance: this });
+
+    console.log(
+      `Thank you ${name}, your reservation is set for ${date} for ${size} people`
+    );
+  }
 }
-
-// export class ReservationBuilder {
-//   constructor(name) {
-//     this.name = name;
-//     console.log(`${name} started a reservation, please select a date`);
-//   }
-
-//   setSize(size) {
-//     console.log(`Checking to see if we have room for ${size} people`);
-
-//     if (calendar.checkAvailability(this.date, size)) {
-//       console.log(`We have room for ${size} people`);
-
-//       this.size = size;
-
-//       return this;
-//     }
-//   }
-
-//   build() {
-//     return new Reservation(this.name, this.date, this.size);
-//   }
-// }
