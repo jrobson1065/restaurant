0084b5f34e4f37c072f47b478648e8a4e32b0cb5
Theo Rafael Cocco <theococco@yahoo.com>
2021-06-02 21:20:48 -0700

    refining restaurant

--- Restaurant/js/helperClasses/reservationHelper.js
+++ Restaurant/js/helperClasses/reservationHelper.js
@@ -1,57 +1,14 @@
-class Reservation {
-  static #existingReservations = [];
+class ReservationBuilder {
+	constructor(id) {
+		this.id = id;
+	}
+	
+	setSize = (size) => this.size = size;
+	setName = (name) => this.name = name;
+	setDate = (date) => this.dateTime = date;
+}
 
-  #makeDate(day, hour) {
-    today = new Date();
-
-    res = new Date(today.getFullYear(), today.getMonth(), day, hour, 0, 0);
-
-    if (today.getDate() > res.getDate()) {
-      if (today.getMonth() === 11)
-        res.setFullYear(today.getFullYear() + 1, today.getMonth() + 1);
-      else res.setMonth(today.getMonth() + 1);
-    }
-
-    return res;
-  }
-
-  checkAvailability = (date, size = 0) => {
-    let seatsTaken = 0;
-
-    Reservation.#existingReservations
-      .filter((r) => r.date === date)
-      .forEach((r) => (seatsTaken += r.size));
-
-    return size ? size + seatsTaken <= 20 : seatsTaken < 20;
-  };
-
-  constructor(name, { date, size }) {
-    this.name = name;
-    console.log(
-      `Thank you ${name} for starting a reservation, please enter the day of the month and the time you wish to set your reservation for.`
-    );
-    this.date = date;
-    this.size = size;
-  }
-
-  setDate(day, hour) {
-    if (calendar.checkHours(day, hour)) {
-      if (this.checkAvailability(this.#makeDate(day, hour))) {
-        console.log(`We still have seats available at `);
-        this.date = this.#makeDate(day, hour);
-      }
-    }
-  }
-
-  setSize(size) {
-    this.size = size;
-  }
-
-  build() {
-    Reservation.#existingReservations.push({ date, size, instance: this });
-
-    console.log(
-      `Thank you ${this.name}, your reservation is set for ${this.date} for ${this.size} people`
-    );
-  }
+class ReservationFactory {
+	id = 1;
+	startReservation = () => new ReservationBuilder(this.id++);
 }
--- Restaurant/js/restaurant.js
+++ Restaurant/js/restaurant.js
@@ -1,5 +1,15 @@
-import { Singleton } from "./helperClasses/singletonHelper";
+import { Singleton } from "./helperClasses/singletonHelper.js";
 
 class Restaurant extends Singleton {
-    
+	normalHours = {open: "5pm", close: "10pm"}
+	hoursOfOperation = {
+		mon: this.normalHours,
+		tue: this.normalHours,
+		wed: this.normalHours,
+		thu: this.normalHours
+	}
+	
+	constructor(name) {
+		this.name = name
+   }
 };
